d4301544cef11167d02ed5037d632f4227e5c027
khoi vu <baokhoi.vu@gmail.com>
2023-10-09 22:16:02 +0700

    add auth for api gateway

---
+++ src/order/order.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Injectable()
+export class OrderService implements OnModuleInit{
+  public onModuleInit(): void {
+    console.log('auth init 12388');
+    // this.svc = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  create(createOrderDto: CreateOrderDto) {
+    return 'This action adds a new order';
+  }
+
+  findAll() {
+    return `This action returns all order`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} order`;
+  }
+}
